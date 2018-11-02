d676744987342beec9dcbcfd5eaf0cc4e6b4c339
DESKTOP-38OQRU0\Amol <amoljp19@gmail.com>
2018-11-02 21:06:51 +0530

    resolved build time errors

---
+++ headyecommerce/app/src/main/java/com/softaai/heady_e_commerce/model/JsonResponseModel.kt
@@ -0,0 +1,9 @@
+package com.softaai.heady_e_commerce.model
+
+
+/**
+ * Created by Amol Pawar on 02-11-2018.
+ * softAai Apps
+ */
+
+data class JsonResponseModel(val userId: Int, val id: Int, val title: String, val body: String)
